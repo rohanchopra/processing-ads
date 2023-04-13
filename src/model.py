f33c558e4a9652be093a5302e7a435eb5087f6ed
Rohan Chopra <git@rchopra.in>
2023-04-12 23:02:38 -0400

    Add efficient net b3

--- src/model.py
+++ src/model.py
@@ -4,25 +4,25 @@ import torch.nn as nn
 
 
 def initialize_model(model_name, num_classes, feature_extract, use_pretrained=None):
-    '''
+    """
     Initialize models for training.
-    '''
+    """
     model_ft = None
     input_size = 0
 
     if model_name == "resnet50":
-        """ 
-            Returns a Resnet 50 model.
+        """
+        Returns a Resnet 50 model.
         """
         model_ft = models.resnet50(weights=use_pretrained)
         set_requires_grad(model_ft, feature_extract)
         num_ftrs = model_ft.fc.in_features
         model_ft.fc = nn.Linear(num_ftrs, num_classes)
         input_size = 232
-        
+
     elif model_name == "resnet34":
-        """ 
-            Returns a Resnet 34 model.
+        """
+        Returns a Resnet 34 model.
         """
         model_ft = models.resnet34(weights=use_pretrained)
         set_requires_grad(model_ft, feature_extract)
@@ -31,8 +31,8 @@ def initialize_model(model_name, num_classes, feature_extract, use_pretrained=No
         input_size = 256
 
     elif model_name == "inceptionv3":
-        """ 
-            Returns an Inception v3 model.
+        """
+        Returns an Inception v3 model.
         """
         model_ft = models.inception_v3(weights=use_pretrained)
         set_requires_grad(model_ft, feature_extract)
@@ -43,10 +43,10 @@ def initialize_model(model_name, num_classes, feature_extract, use_pretrained=No
         num_ftrs = model_ft.fc.in_features
         model_ft.fc = nn.Linear(num_ftrs, num_classes)
         input_size = 299
-        
+
     elif model_name == "vgg16":
-        """ 
-            Returns a VGG 16 model with batch normalization.
+        """
+        Returns a VGG 16 model with batch normalization.
         """
         model_ft = models.vgg16_bn(weights=use_pretrained)
         set_requires_grad(model_ft, feature_extract)
@@ -55,8 +55,8 @@ def initialize_model(model_name, num_classes, feature_extract, use_pretrained=No
         input_size = 256
 
     elif model_name == "mobile_net_v3_large":
-        """ 
-            returns a mobile net V3 large model.
+        """
+        returns a mobile net V3 large model.
         """
         model_ft = models.mobilenet_v3_large(weights=use_pretrained)
         set_requires_grad(model_ft, feature_extract)
@@ -65,8 +65,8 @@ def initialize_model(model_name, num_classes, feature_extract, use_pretrained=No
         input_size = 232
 
     elif model_name == "efficient_net_b1":
-        """ 
-            Returns an efficient net b1 model.
+        """
+        Returns an efficient net b1 model.
         """
         model_ft = models.efficientnet_b1(weights=use_pretrained)
         set_requires_grad(model_ft, feature_extract)
@@ -75,14 +75,24 @@ def initialize_model(model_name, num_classes, feature_extract, use_pretrained=No
         input_size = 255
 
     elif model_name == "efficient_net_b0":
-        """ 
-            Returns an efficient net b0 model.
+        """
+        Returns an efficient net b0 model.
         """
         model_ft = models.efficientnet_b0(weights=use_pretrained)
         set_requires_grad(model_ft, feature_extract)
         num_ftrs = model_ft.classifier[1].in_features
         model_ft.classifier[1] = nn.Linear(num_ftrs, num_classes, bias=True)
         input_size = 256
+
+    elif model_name == "efficient_net_b3":
+        """
+        Returns an efficient net b3 model.
+        """
+        model_ft = models.efficientnet_b3(weights=use_pretrained)
+        set_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[1].in_features
+        model_ft.classifier[1] = nn.Linear(num_ftrs, num_classes, bias=True)
+        input_size = 320
 
     else:
         print("Unavailable model selected.")
