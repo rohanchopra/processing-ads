e9fae4c9693c1aecb2c707b399a5aee6d1500d3e
Rohan Chopra <git@rchopra.in>
2023-04-12 23:02:27 -0400

    Clean up the code

--- src/multilabel/data.py
+++ src/multilabel/data.py
@@ -1,67 +1,82 @@
 import os
 import pandas as pd
 import torchvision.transforms as T
-from torchvision import datasets
 import torch.utils.data as td
 from multilabel.loader import MultiLabelDataset
 
 
-def load_data(path, batch_size, input_size, norm_arr, 
-              num_workers=0):
-    
-    transform_dict = {"train": 
-                          T.Compose([
-                            #T.ToPILImage(),
-                            T.Resize(size=(input_size, input_size)),
+def load_data(path, batch_size, input_size, norm_arr, num_workers=0):
 
-                            T.RandomHorizontalFlip(),
-                            #T.ColorJitter(contrast=0.5),
-                            #T.RandomAdjustSharpness(2),
-                            #T.RandomAutocontrast(),
+    transform_dict = {
+        "train": T.Compose(
+            [
+                # T.ToPILImage(),
+                T.Resize(size=(input_size, input_size)),
+                T.RandomHorizontalFlip(),
+                # T.ColorJitter(contrast=0.5),
+                # T.RandomAdjustSharpness(2),
+                # T.RandomAutocontrast(),
+                T.ToTensor(),
+                T.Normalize(*norm_arr),
+            ]
+        ),
+        "test_val": T.Compose(
+            [
+                # T.ToPILImage(),
+                T.Resize(size=(input_size, input_size)),
+                T.ToTensor(),
+                T.Normalize(*norm_arr),
+            ]
+        ),
+    }
 
-                            T.ToTensor(),
-                            T.Normalize(*norm_arr)]),
-                        "test_val": 
-                          T.Compose([
-                            #T.ToPILImage(),
-                            T.Resize(size=(input_size, input_size)),
-                            T.ToTensor(),
-                            T.Normalize(*norm_arr)])
-                        }
-    
-    train = pd.read_csv(os.path.join(path, "train.csv")).set_index('Path').fillna(0)
-    test = pd.read_csv(os.path.join(path, "test.csv")).set_index('Path').fillna(0)
-    val = pd.read_csv(os.path.join(path, "val.csv")).set_index('Path').fillna(0)
-    
-    train_dataset = MultiLabelDataset(root=os.path.join(path, "images", "train"),
-                                         dataframe=train,
-                                         transform=transform_dict["train"])
-    val_dataset = MultiLabelDataset(root=os.path.join(path, "images", "val"),
-                                       dataframe=val,
-                                       transform=transform_dict["test_val"])
-    test_dataset = MultiLabelDataset(root=os.path.join(path, "images", "test"),
-                                        dataframe=test,
-                                        transform=transform_dict["test_val"])
-    
-    data_loader_train = td.DataLoader(train_dataset,
-                                      batch_size=batch_size,
-                                      shuffle=True,
-                                      drop_last=False,
-                                      num_workers=num_workers,
-                                      pin_memory=True)
-    data_loader_val = td.DataLoader(val_dataset,
-                                    batch_size=batch_size,
-                                    shuffle=False,
-                                    drop_last=False,
-                                    num_workers=num_workers,
-                                    pin_memory=True)
-    data_loader_test = td.DataLoader(test_dataset,
-                                     batch_size=batch_size,
-                                     shuffle=False,
-                                     drop_last=False,
-                                     num_workers=num_workers,
-                                     pin_memory=True)
+    train = pd.read_csv(os.path.join(path, "train.csv")).set_index("Path").fillna(0)
+    test = pd.read_csv(os.path.join(path, "test.csv")).set_index("Path").fillna(0)
+    val = pd.read_csv(os.path.join(path, "val.csv")).set_index("Path").fillna(0)
 
-    return {'train': data_loader_train,
-            'val':   data_loader_val,
-            'test':  data_loader_test}
+    train_dataset = MultiLabelDataset(
+        root=os.path.join(path, "images", "train"),
+        dataframe=train,
+        transform=transform_dict["train"],
+    )
+    val_dataset = MultiLabelDataset(
+        root=os.path.join(path, "images", "val"),
+        dataframe=val,
+        transform=transform_dict["test_val"],
+    )
+    test_dataset = MultiLabelDataset(
+        root=os.path.join(path, "images", "test"),
+        dataframe=test,
+        transform=transform_dict["test_val"],
+    )
+
+    data_loader_train = td.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        drop_last=False,
+        num_workers=num_workers,
+        pin_memory=True,
+    )
+    data_loader_val = td.DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        drop_last=False,
+        num_workers=num_workers,
+        pin_memory=True,
+    )
+    data_loader_test = td.DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        drop_last=False,
+        num_workers=num_workers,
+        pin_memory=True,
+    )
+
+    return {
+        "train": data_loader_train,
+        "val": data_loader_val,
+        "test": data_loader_test,
+    }
--- src/utils.py
+++ src/utils.py
@@ -6,14 +6,16 @@ def set_requires_grad(model, feature_extracting):
     if feature_extracting:
         for param in model.parameters():
             param.requires_grad = False
-            
-            
+
+
 def save_model(model, path, name):
     torch.save(model, os.path.join(path, name))
     return True
 
 
-def encode_label(label, classes_list): #encoding the classes into a tensor of shape (11) with 0 and 1s.
+def encode_label(
+    label, classes_list
+):  # encoding the classes into a tensor of shape (11) with 0 and 1s.
     target = torch.zeros(len(classes_list))
     for l in label:
         idx = classes_list.index(l)
@@ -21,9 +23,11 @@ def encode_label(label, classes_list): #encoding the classes into a tensor of sh
     return target
 
 
-def decode_target(classes, target, threshold=0.5): #decoding the prediction tensors of 0s and 1s into text form
+def decode_target(
+    classes, target, threshold=0.5
+):  # decoding the prediction tensors of 0s and 1s into text form
     result = []
     for i, x in enumerate(target):
-        if (x >= threshold):
-            result.append(classes[i])     
-    return ' '.join(result)
+        if x >= threshold:
+            result.append(classes[i])
+    return " ".join(result)
